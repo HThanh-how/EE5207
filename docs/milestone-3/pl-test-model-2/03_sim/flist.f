c67661c61078f8222cd6a25e5dbe081d15d7ba97
Dang Nguyen <115719434+DangNguyenHong@users.noreply.github.com>
2025-12-13 13:33:19 +0700

    Add files via upload

---
+++ docs/milestone-3/pl-test-model-2/03_sim/flist.f
@@ -0,0 +1,16 @@
+-sv
+-timescale 1ns/100ps
+
+# SOURCE CODE FILES
+./../00_src/alu.sv
+./../00_src/control_unit.sv
+./../00_src/dmem_sync.sv
+./../00_src/imem_sync.sv
+./../00_src/pipelined.sv
+./../00_src/register_file.sv
+
+# TESTBENCH FILES
+./../01_bench/driver.sv
+./../01_bench/scoreboard.sv
+./../01_bench/tbench.sv
+./../01_bench/tlib.svh
