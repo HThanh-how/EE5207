6ac10f67e1bd979720162382b002dda9fcaff6be
Giang <giang0351@gmail.com>
2025-12-21 20:22:41 +0700

    feat: Update scoreboard module to implement dual methods for LEDR output tracking based on PC value and changes

--- docs/milestone-3/pl-test-model-1/01_bench/scoreboard.sv
+++ docs/milestone-3/pl-test-model-1/01_bench/scoreboard.sv
@@ -60,11 +60,18 @@ module scoreboard(
 
 
   always @(negedge i_clk) begin : debug
-      // In milestone-3, o_pc_debug = wb_pc (WB stage PC)
-      // But I/O write happens in MEM stage, so we need to catch LEDR changes
-      // Print character when LEDR[7:0] changes and is not zero
-      if (!i_reset && (o_io_ledr[7:0] != prev_ledr) && (o_io_ledr[7:0] != 8'b0)) begin
-          $write("%s", o_io_ledr[7:0]);
+      if (!i_reset) begin
+          // Method 1: Check PC = 0x18 (like milestone-2, but o_pc_debug = wb_pc in milestone-3)
+          // When instruction at PC 0x18 reaches WB stage, o_pc_debug = 0x18
+          // At that time, o_io_ledr should already have the value written in MEM stage
+          if (o_pc_debug == 32'h18) begin
+              $write("%s", o_io_ledr[7:0]);
+          end
+          // Method 2: Also catch LEDR changes as backup
+          // This catches I/O writes that happen at other PCs
+          else if ((o_io_ledr[7:0] != prev_ledr) && (o_io_ledr[7:0] != 8'b0)) begin
+              $write("%s", o_io_ledr[7:0]);
+          end
       end
   end
 
